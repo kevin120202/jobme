17eb3ff29b9b26be88ef613fdd02bd2a4644e961
Kevin Dela Paz <kevindelapaz21@gmail.com>
2024-07-05 11:43:08 -0500

    added get single job route

--- server.js
+++ server.js
@@ -39,7 +39,17 @@ app.post("/api/v1/jobs", (req, res) => {
     const id = nanoid(10)
     const job = { id, company, position }
     jobs.push(job)
-    res.status(200).json({ jobs })
+    res.status(201).json({ jobs })
+})
+
+// GET SINGLE JOB
+app.get("/api/v1/jobs/:id", (req, res) => {
+    const { id } = req.params
+    const job = jobs.find(job => job.id === id)
+    if (!job) {
+        return res.status(404).json({ msg: `no job with id ${id}` })
+    }
+    res.status(200).json({ job })
 })
 
 const port = process.env.PORT || 5100
