d03bd168240ea28447b9d47886b4bcfb8874fa8d
Kevin Dela Paz <kevindelapaz21@gmail.com>
2024-07-13 09:31:37 -0500

    fixed update user

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,15 +1,17 @@
 import multer from "multer";
+import DataParser from "datauri/parser.js"
+import path, { parse } from "path"
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'public/uploads')
-    },
-    filename: (req, file, cb) => {
-        const fileName = file.originalname
-        cb(null, fileName)
-    }
-})
+const storage = multer.memoryStorage()
 
 const upload = multer({ storage })
+
+const parser = new DataParser()
+
+export const formatImage = (file) => {
+    console.log(file);
+    const fileExtension = path.extname(file.originalname).toString()
+    return parser.format(fileExtension, file.buffer).content
+}
 
 export default upload
