5bb8647b0c0c6016001253cf7378ac5abdebd575
Kevin Dela Paz <kevindelapaz21@gmail.com>
2024-07-24 18:21:48 -0500

    changed label text for location

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -31,7 +31,7 @@ export default function AddJob() {
                 <div className="form-center">
                     <FormRow type="text" name='position' />
                     <FormRow type="text" name='company' />
-                    <FormRow type="text" name='jobLocation' labelText='job' defaultValue={user.location} />
+                    <FormRow type="text" name='jobLocation' labelText='job location' defaultValue={user.location} />
                     <FormRowSelect name="jobStatus" labelText="job status" list={Object.values(JOB_STATUS)} defaultValue={JOB_STATUS.PENDING} />
                     <FormRowSelect name="jobType" labelText="job type" list={Object.values(JOB_TYPE)} defaultValue={JOB_TYPE.FULL_TIME} />
                     <button type="submit" className='btn btn-block form-btn' disabled={isSubmitting}>{isSubmitting ? "submitting" : "submit"}</button>
